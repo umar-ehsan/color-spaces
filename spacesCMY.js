41875455331b4d396ed8184c87dc42e99cb6ed31
umar-ehsan <muhammad.ehsan@uoit.net>
2017-10-29 12:40:52 -0400

    Author: M. Umar Ehsan   Version : 1.0
      Project: A simple implementation of the CMY color space mixing in
      HTML, CSS and JavaScript.

---
+++ spacesCMY.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", function() {
+    var magentaslide = document.getElementById("magentaslider");
+    var yellowslide = document.getElementById("yellowslider");
+    var cyanslide = document.getElementById("cyanslider");
+
+    var valmagenta = document.getElementById('magentaval');
+    var valyellow = document.getElementById("yellowval");
+    var valcyan = document.getElementById("cyanval");
+
+	process();
+
+    magentaslide.addEventListener("input",process);
+    yellowslide.addEventListener("input",process);
+    cyanslide.addEventListener("input",process);
+
+    function process(){
+
+        valmagenta.innerHTML = magentaslide.value;
+        valyellow.innerHTML = yellowslide.value;
+        valcyan.innerHTML = cyanslide.value;
+
+        var k1 = parseInt(magentaslide.value*255);
+        var k2 = parseInt(yellowslide.value*255);
+        var k3 = parseInt(cyanslide.value*255);
+
+        var colors = cmyToRgb([k1,k2,k3]);
+
+        document.getElementById("k1val").innerHTML = magentaslide.value;
+        document.getElementById("k2val").innerHTML = yellowslide.value;
+        document.getElementById("k3val").innerHTML = cyanslide.value;
+
+
+
+        document.getElementById("bigcircle").style.backgroundColor = "rgb("+colors[0]+","+colors[1]+","+colors[2]+")";
+
+    }
+	
+	function cmyToRgb(color){
+		return [255 - color[0],255 - color[1],255 - color[2]];
+	}
+	
+	
+});
+
+
+
